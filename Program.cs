9a05f4390c437356d044450cd332915dd188e935
Jakub Owczarek <jakub.owczarek@vip.onet.pl>
2024-04-10 17:59:42 +0200

    clear

--- Program.cs
+++ Program.cs
@@ -1,9 +1,1 @@
-﻿string message = "Find what is (inside the parentheses)";
-
-int openingPosition = message.IndexOf('(');
-int closingPosition = message.IndexOf(')');
-
-openingPosition += 1;
-
-int length = closingPosition - openingPosition;
-Console.WriteLine(message.Substring(openingPosition, length));
+﻿

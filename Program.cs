dc2ea9bfb241cd2c9f84166b768b6264c8e2a26c
Jakub Owczarek <jakub.owczarek@vip.onet.pl>
2024-04-10 14:34:50 +0200

    challange

--- Program.cs
+++ Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿string pangram = "The quick brown fox jumps over the lazy dog";
+
+string[] words = pangram.Split(" ");
+var result = "";
+foreach (var word in words)
+{
+    result += String.Join("", word.Reverse());
+    result += String.Join("", " ");
+}
+
+Console.WriteLine(result);

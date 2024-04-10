2e535d0b5093a58ef9d0259012f7d75e8c3eb79c
Jakub Owczarek <jakub.owczarek@vip.onet.pl>
2024-04-10 15:42:46 +0200

    exercise

--- Program.cs
+++ Program.cs
@@ -5,3 +5,7 @@ decimal taxPercentage = .15825m;
 decimal total = 3185.19m;
 
 Console.WriteLine($"Invoice Number: {invoiceNumber}");
+Console.WriteLine($"Shares: {productShares:N3}");
+Console.WriteLine($"Sub Total: {subtotal:C}");
+Console.WriteLine($"taxPercentage: {taxPercentage:P2}");
+Console.WriteLine($"Total: {total:C}");

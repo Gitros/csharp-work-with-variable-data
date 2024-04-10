507e69ab387811e515f85e162ed3f8c6908fd71f
Jakub Owczarek <jakub.owczarek@vip.onet.pl>
2024-04-10 18:01:30 +0200

    LastIndexOf()

--- Program.cs
+++ Program.cs
@@ -1,1 +1,7 @@
-﻿
+﻿string message = "(What if) I am (only interested) in the last (set of parentheses)?";
+int openingPosition = message.LastIndexOf('(');
+
+openingPosition += 1;
+int closingPosition = message.LastIndexOf(')');
+int length = closingPosition - openingPosition;
+Console.WriteLine(message.Substring(openingPosition, length));

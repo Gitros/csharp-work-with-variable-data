74294a5786521ec1e7872ddeeba1ac6c8633b2c2
Jakub Owczarek <jakub.owczarek@vip.onet.pl>
2024-04-10 15:24:48 +0200

    clear

--- Program.cs
+++ Program.cs
@@ -1,16 +1,1 @@
-﻿string orderStream = "B123,C234,A345,C15,B177,G3003,C235,B179";
-
-string[] orderIDs = orderStream.Split(",");
-Array.Sort(orderIDs);
-
-foreach (var orderID in orderIDs)
-{
-    if (orderID.Length == 4)
-    {
-        Console.WriteLine(orderID);
-    }
-    else
-    {
-        Console.WriteLine($"{orderID}\t - Error");
-    }
-}
+﻿

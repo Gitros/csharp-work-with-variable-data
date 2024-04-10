13e3215b7037c236a5a9d94aa2c4c78d47e9b3a6
Jakub Owczarek <jakub.owczarek@vip.onet.pl>
2024-04-10 14:37:52 +0200

    clear

--- Program.cs
+++ Program.cs
@@ -1,11 +1,1 @@
-﻿string pangram = "The quick brown fox jumps over the lazy dog";
-
-string[] words = pangram.Split(" ");
-var result = "";
-foreach (var word in words)
-{
-    result += String.Join("", word.Reverse());
-    result += String.Join("", " ");
-}
-
-Console.WriteLine(result);
+﻿

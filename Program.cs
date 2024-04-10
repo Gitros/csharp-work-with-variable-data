4c1996e561256dab794154d47a0f412b4b99903f
Jakub Owczarek <jakub.owczarek@vip.onet.pl>
2024-04-10 15:38:08 +0200

    clear

--- Program.cs
+++ Program.cs
@@ -1,7 +1,1 @@
-﻿decimal price = 67.55m;
-decimal salePrice = 59.99m;
-
-string yourDiscount = String.Format("You saved {0:C2} off the regular {1:C2} price. ", (price - salePrice), price);
-
-yourDiscount += $"A discount of {((price - salePrice) / price):P2}!"; //inserted
-Console.WriteLine(yourDiscount);
+﻿

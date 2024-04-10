ef246a9f8773bd22eba3ce055c713a042528c959
Jakub Owczarek <jakub.owczarek@vip.onet.pl>
2024-04-10 15:42:58 +0200

    clear

--- Program.cs
+++ Program.cs
@@ -1,11 +1,1 @@
-﻿int invoiceNumber = 1201;
-decimal productShares = 25.4568m;
-decimal subtotal = 2750.00m;
-decimal taxPercentage = .15825m;
-decimal total = 3185.19m;
-
-Console.WriteLine($"Invoice Number: {invoiceNumber}");
-Console.WriteLine($"Shares: {productShares:N3}");
-Console.WriteLine($"Sub Total: {subtotal:C}");
-Console.WriteLine($"taxPercentage: {taxPercentage:P2}");
-Console.WriteLine($"Total: {total:C}");
+﻿

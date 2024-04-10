f0fac7b6784331883dec24023aaf797367a6dbda
Jakub Owczarek <jakub.owczarek@vip.onet.pl>
2024-04-10 15:34:41 +0200

    combining formating

--- Program.cs
+++ Program.cs
@@ -1,1 +1,6 @@
-﻿
+﻿decimal price = 67.55m;
+decimal salePrice = 59.99m;
+
+string yourDiscount = String.Format("You saved {0:C2} off the regular {1:C2} price. ", (price - salePrice), price);
+
+Console.WriteLine(yourDiscount);
